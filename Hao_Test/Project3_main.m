ec20828eb2a78f90bcd4ac7153f73a03a3d064cf
Hao Li <li.138@wright.edu>
2016-12-08 16:56:00 -0500

    Update

--- Hao_Test/Project3_main.m
+++ Hao_Test/Project3_main.m
@@ -42,15 +42,38 @@ w = sqrt(values); % natural frequency
 Phi = (L')\vectors_V;
 
 %% Calculate C
-zeta = 0.02;
+zeta = 0.02;    % damping ratio
 C = Phi'\diag(2*zeta*w)*inv(Phi);
 
 % Testing Phi
-Phi'*Mr*Phi
-Phi'*Kr*Phi
-Phi'*C*Phi
+% Phi'*Mr*Phi
+% Phi'*Kr*Phi
+% Phi'*C*Phi
 
 %% Newmark beta method
+beta = 1/4;
+gama = 1/2;
+% Omega_crit = (zeta*(gama-1/2)+sqrt(gama/2-beta+zeta^2*(gama-1/2)^2))/(gama/2-beta);
+% detaT = 0.5*Omega_crit/max(w);
+t = 0.01; 
+step = 100;
+dt = 0.01/step;
+
+D = zeros(length(Kr),1);    % Displacement
+Dd = D;                     % Velocity
+Ddd = D;                    % Acceleration
+
+R = zeros(length(Kr),1);
+R(end-1) = 100000;  % Load vector
+for i = 1:step
+    Di = (1/(beta*dt^2)*Mr + gama/(beta*dt)*C +Kr)\...
+        (R+Mr*(1/(beta*dt^2)*D(:,i) + 1/(beta*dt)*Dd(:,i) + (1/(2*beta)-1)*Ddd(:,i)) ...
+        + C*( gama/(beta*dt)*D(:,i) + (gama/beta-1)*Dd(:,i) + (gama/beta-2)*dt/2*Ddd(:,i)));
+    Ddi = gama/(beta*dt)*(Di-
+    D = [D;Di];
+    
+end
+
 
 
 
