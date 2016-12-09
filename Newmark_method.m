b605bbced38ae949d221052a9332b52c72dc63f8
EngMohammed89 <ingenium_mj@yahoo.com>
2016-12-09 17:20:33 -0500

    update

--- Newmark_method.m
+++ Newmark_method.m
@@ -1,12 +1,12 @@
 % Newmark method
 load K_M.mat % Load M and K matrieces
 [Kr, Mr, C, wmax] = find_C(K,M); 
-C=0
+
 Inv=eye(150,150); % Identity matrix will be used to take inverse of [150x150] matrix
 
-% % Average acceleration
-% beta = 1/4; % beta
-% gamma = 1/2; % gamma
+% Average acceleration
+beta = 1/4; % beta
+gamma = 1/2; % gamma
 
 % % Linear acceleration
 % beta = 1/6; % beta
@@ -16,9 +16,9 @@ Inv=eye(150,150); % Identity matrix will be used to take inverse of [150x150] ma
 % beta = 1/12; % beta
 % gamma = 1/2; % gamma
 
-% Algorithmically damped
-gamma = 0.6; % gamma
-beta = (1/4)*(gamma+0.5)^2; % beta
+% % Algorithmically damped
+% gamma = 0.6; % gamma
+% beta = (1/4)*(gamma+0.5)^2; % beta
 
 % % Hilber-hughes-Taylor (alpha-method)
 % alpha =-0.2; % -1/3 <= alpha <=0
@@ -31,7 +31,7 @@ beta = (1/4)*(gamma+0.5)^2; % beta
 % Ocrit=(Z*(gamma-0.5)+sqrt((gamma/2)-beta+(Z^2)*(gamma-0.5)^2))/(gamma/2-beta);
 % dt=Ocrit/wmax;
 
-dt = 0.0001; % delta t. use this for above methods (1st, 4th and 5th).
+dt = 0.0001; % delta t. use this for above 3 methods (1st, 4th and 5th).
 tf = 0.15; % Final t
 n=floor(tf/dt); % Steps
 t=zeros(n,1); % Time
@@ -75,16 +75,16 @@ grid on; hold on
 plot(t,D(121,:)) % Displacement of the rotational DOF at node 41
 ylabel('$\theta z41$ (rad/s)','interpreter','latex')
 xlabel('t (s)','interpreter','latex')
-title('Hilber-hughes-Taylor (\alpha-method)')
+title('Average acceleration')
 figure(2)
 grid on; hold on
 plot(t,DD(121,:)) % Velocity of the rotational DOF at node 41
 ylabel('$\dot{\theta}z41$ (rad/s)','interpreter','latex')
 xlabel('t (s)','interpreter','latex')
-title('Hilber-hughes-Taylor (\alpha-method)')
+title('Average acceleration')
 figure(3)
 grid on; hold on
 plot(t,DDD(121,:)) % Acceleration of the rotational DOF at node 41
 ylabel('$\ddot{\theta}z41$ (rad/s)','interpreter','latex')
 xlabel('t (s)','interpreter','latex')
-title('Hilber-hughes-Taylor (\alpha-method)')
+title('Average acceleration')
